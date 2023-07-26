342f57d85f9f9b1dfd49c9d130d140f3bfdf1858
PT2911p <pinaktrivedi213@gmail.com>
2023-07-26 17:02:20 +0530

    Deployment

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
- const API_URI = "http://127.0.0.1:8000";
+ const API_URI = "https://online-judge-node01.onrender.com";
 
 export const Register = async (data) => {
     try {
